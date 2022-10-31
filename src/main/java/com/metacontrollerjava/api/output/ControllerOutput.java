782433f986e6b4175fd653e9d603d587bf8a7771
Cora Iberkleid <ciberkleid@gmail.com>
2022-10-31 08:23:30 -0400

    replaced Status with Generic

--- src/main/java/com/metacontrollerjava/api/output/ControllerOutput.java
+++ src/main/java/com/metacontrollerjava/api/output/ControllerOutput.java
@@ -1,9 +1,8 @@
 package com.metacontrollerjava.api.output;
 
-import com.metacontrollerjava.api.output.Status;
 import io.kubernetes.client.common.KubernetesObject;
 
 import java.util.List;
 
-public record ControllerOutput(List<KubernetesObject> children, Status status) {
+public record ControllerOutput<T>(List<KubernetesObject> children, T status) {
 }
--- src/main/java/com/metacontrollerjava/api/output/Status.java
+++
@@ -1,6 +0,0 @@
-package com.metacontrollerjava.api.output;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record Status(@JsonProperty("prod-tests") boolean productionTestEnabled, boolean ready, String url) {
-}
